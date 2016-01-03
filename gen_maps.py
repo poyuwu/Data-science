1388858dc725a43cfb01d5f8e308c72583e6973d
playif1 <playif1@gmail.com>
2016-01-03 06:00:41 -0800

    new maps

--- gen_maps.py
+++ gen_maps.py
@@ -3,8 +3,7 @@ import par
 
 final = par.parse_json()
 mymap = pygmaps.maps(22.69781, 120.960515, 8)
-#for i in range(440,445):
 for i in range(len(final)):
     if (final[i]['x1'] != '') and (final[i]['y1'] != ''):
         mymap.addpoint(final[i], "#0000FF")
-mymap.draw('./mymap.html')
+mymap.draw('./index.html')
